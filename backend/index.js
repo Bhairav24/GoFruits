8602722453cb8d915e32b33d6e861e2ae0512c87
Bhairav1 <gawandebb@rknec.edu>
2023-12-12 23:37:03 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@
 
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT||5000;
 const cors = require('cors');
 const mongoDB = require('./db.js');
 
